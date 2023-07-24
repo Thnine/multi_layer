0a66777582a47a3761de66c5d8612e690c44dfaa
Thnine <775903190@qq.com>
2023-07-24 08:45:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div style="width:600px;height:1000px">
+      <MultiLayer/>
+    </div>
+  </div>
+</template>
+
+<script>
+import MultiLayer from './components/MultiLayer'
+
+export default {
+  name: 'App',
+  components: {
+    MultiLayer
+  },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MultiLayer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="multi_layer_container">
+    <svg class="muiti_layer_canva"></svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MultiLayer',
+  data () {
+    return {
+      raw_node:[],//[{'index':'1',...},{'index':'2',...},...]
+      raw_link:[],//[{'index':'1',...},{'index':'2',...}]
+    }
+  },
+  methods:{
+    setData(nodes,links){//绑定数据
+      this.raw_node = JSON.parse(JSON.stringify(nodes));
+      this.raw_link = JSON.parse(JSON.stringify(links));
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .multi_layer_container{
+    height:100%;
+    width: 100%;
+    overflow: auto;
+  }
+  .muiti_layer_canva{
+    width:100%;
+  }
+</style>
